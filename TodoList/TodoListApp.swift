567e75a5f90b1cf573caa68e34548210e03071fa
Armando <jarodriguezvera@gmail.com>
2023-08-07 10:49:26 -0600

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Armando Rodríguez on 07/08/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
